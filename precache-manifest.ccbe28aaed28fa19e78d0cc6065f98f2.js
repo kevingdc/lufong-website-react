7e0d9c403d3896e22d4df1b2df3157992b47537f
Kevin Chan <kevin98gray@gmail.com>
2019-07-03 18:41:07 +0800

    Updates

---
+++ precache-manifest.ccbe28aaed28fa19e78d0cc6065f98f2.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "663d45c1ee57b8412dd762d8b2c60541",
+    "url": "/lufong-website-react/index.html"
+  },
+  {
+    "revision": "b0243d9d641eef7ecfef",
+    "url": "/lufong-website-react/static/css/main.fbbc5718.chunk.css"
+  },
+  {
+    "revision": "a322755ff48935eca12e",
+    "url": "/lufong-website-react/static/js/2.514b919f.chunk.js"
+  },
+  {
+    "revision": "b0243d9d641eef7ecfef",
+    "url": "/lufong-website-react/static/js/main.75086a19.chunk.js"
+  },
+  {
+    "revision": "15bf1bddf9d39393ace6",
+    "url": "/lufong-website-react/static/js/runtime~main.27d90415.js"
+  },
+  {
+    "revision": "0176ab3292c391aacac3077e155463ca",
+    "url": "/lufong-website-react/static/media/birthday-promos-1.0176ab32.jpg"
+  },
+  {
+    "revision": "f2ebd86a5382bc9d2c87d64b897d3b92",
+    "url": "/lufong-website-react/static/media/braised-beef-poster.f2ebd86a.jpg"
+  },
+  {
+    "revision": "aa30d018f92a93044c7e2a7385a3dc61",
+    "url": "/lufong-website-react/static/media/carousel-1.aa30d018.jpg"
+  },
+  {
+    "revision": "5d9458310230748838e0dacfa8affec9",
+    "url": "/lufong-website-react/static/media/carousel-2.5d945831.jpg"
+  },
+  {
+    "revision": "0d170229676380ef6b376f1690c281dc",
+    "url": "/lufong-website-react/static/media/chinese-fried-chicken-poster.0d170229.jpg"
+  },
+  {
+    "revision": "526e11bda93ad27a52567ca7f67da4cf",
+    "url": "/lufong-website-react/static/media/chinese-fried-chicken.526e11bd.jpg"
+  },
+  {
+    "revision": "e524c5cf8f571c698e26c80992856653",
+    "url": "/lufong-website-react/static/media/chopsuey-con-lechon.e524c5cf.jpg"
+  },
+  {
+    "revision": "7ece275c7dffaf29becd9c72cc1a2b2f",
+    "url": "/lufong-website-react/static/media/chopsuey-guisado.7ece275c.jpg"
+  },
+  {
+    "revision": "6a61fd098974e9df4892c3c75b773a34",
+    "url": "/lufong-website-react/static/media/japanese-siomai.6a61fd09.jpg"
+  },
+  {
+    "revision": "68497a75357931b1c7fe816d53538fde",
+    "url": "/lufong-website-react/static/media/lechon-kawali-poster.68497a75.jpg"
+  },
+  {
+    "revision": "17e52f1c87c259cd02c6459414a0b1c6",
+    "url": "/lufong-website-react/static/media/lechon-kawali.17e52f1c.jpg"
+  },
+  {
+    "revision": "f65392ea21c52776d7f8169c6fc46480",
+    "url": "/lufong-website-react/static/media/lufong-logo.f65392ea.png"
+  },
+  {
+    "revision": "28305f6a562243ebb065fcb363ea31e3",
+    "url": "/lufong-website-react/static/media/lufong-pansit-king.28305f6a.png"
+  },
+  {
+    "revision": "65d196a931db6ad4caf930cbb1e303a3",
+    "url": "/lufong-website-react/static/media/lumpiang-shanghai-poster.65d196a9.jpg"
+  },
+  {
+    "revision": "968d63d07fc43dded3eb2b144c43e540",
+    "url": "/lufong-website-react/static/media/lumpiang-shanghai.968d63d0.jpg"
+  },
+  {
+    "revision": "9c829fb1a14d1e6d44484326a93c9c76",
+    "url": "/lufong-website-react/static/media/pancit-king.9c829fb1.jpg"
+  },
+  {
+    "revision": "7acdf33de3372842c5d75af634c9ef1c",
+    "url": "/lufong-website-react/static/media/pancit-queen.7acdf33d.jpg"
+  },
+  {
+    "revision": "52aca21bd0a216ecf55d9fc1e14dcaba",
+    "url": "/lufong-website-react/static/media/pancit-regular.52aca21b.jpg"
+  },
+  {
+    "revision": "3d5c79e4215f0461fd4c9f425bdc3259",
+    "url": "/lufong-website-react/static/media/pinsec-prito.3d5c79e4.jpg"
+  },
+  {
+    "revision": "11a81a68ff32f94e93a9eaeefcb4d7b2",
+    "url": "/lufong-website-react/static/media/pork-siomai.11a81a68.jpg"
+  },
+  {
+    "revision": "005bc6eb399deb2e75160a4322230807",
+    "url": "/lufong-website-react/static/media/shanghai-fried-rice.005bc6eb.jpg"
+  },
+  {
+    "revision": "98abacfa8c7cfc26208a107822d5899b",
+    "url": "/lufong-website-react/static/media/solo-meals-1.98abacfa.jpg"
+  },
+  {
+    "revision": "d82db0cc91f38889100d209b3fdd281d",
+    "url": "/lufong-website-react/static/media/solo-meals-2.d82db0cc.jpg"
+  },
+  {
+    "revision": "cc3cb8a39942591ae29f108a89497b1f",
+    "url": "/lufong-website-react/static/media/solo-meals-3.cc3cb8a3.jpg"
+  },
+  {
+    "revision": "1e35abae12ed79759ef04be0552afed7",
+    "url": "/lufong-website-react/static/media/solo-meals-4.1e35abae.jpg"
+  },
+  {
+    "revision": "3086677b39dc0c07687d6f259f2f2391",
+    "url": "/lufong-website-react/static/media/solo-meals-5.3086677b.jpg"
+  },
+  {
+    "revision": "7529996529c94f2b4f11ee070836dd3c",
+    "url": "/lufong-website-react/static/media/sulit-sarap-1-poster.75299965.jpg"
+  },
+  {
+    "revision": "ea0b26b068e24e3731f06abc9439034c",
+    "url": "/lufong-website-react/static/media/sulit-sarap-2-poster.ea0b26b0.jpg"
+  },
+  {
+    "revision": "934721e94e4a42d1521c59c6c530a8d6",
+    "url": "/lufong-website-react/static/media/sulit-sarap-3-poster.934721e9.jpg"
+  },
+  {
+    "revision": "cf256c234af349a516520d8b5e9e5795",
+    "url": "/lufong-website-react/static/media/sulit-sarap-promos-1.cf256c23.jpg"
+  },
+  {
+    "revision": "ac739009299b7153f2ba6b1ca3481198",
+    "url": "/lufong-website-react/static/media/sulit-sarap-promos-2.ac739009.jpg"
+  },
+  {
+    "revision": "973308d0009f2ed73b16b3b3f00f5b49",
+    "url": "/lufong-website-react/static/media/sulit-sarap-promos-3.973308d0.jpg"
+  },
+  {
+    "revision": "913b5f68dae9e9ff3a77e387cc5fa7b6",
+    "url": "/lufong-website-react/static/media/ube-butchi.913b5f68.jpg"
+  },
+  {
+    "revision": "f11ffcfe9401062a70c490b7f24ce465",
+    "url": "/lufong-website-react/static/media/ube-pao.f11ffcfe.jpg"
+  },
+  {
+    "revision": "0a701616f191de5cdb806b3fa43d1524",
+    "url": "/lufong-website-react/static/media/yangchow-fried-rice.0a701616.jpg"
+  }
+]);
